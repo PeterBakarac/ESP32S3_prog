a49ea921a19966729958038b5ef30d12d045c394
PeterBakaracSTU <peter.bakarac@stuba.sk>
2024-03-28 17:56:16 +0100

    DI_5_prog

--- Examples/digital_IO5/ex_1/ex_1.ino
+++ Examples/digital_IO5/ex_1/ex_1.ino
@@ -1,10 +1,10 @@
-const uint8_t BUZZER_PIN = 48;
-unsigned int BUZZER_FREQUENCY = 14E3;
-unsigned long BUZZER_DURATION = 5E3;
+const uint8_t buzzer_pin = 48;
+unsigned int buzzer_frequency = 9E3;
+unsigned long buzzer_duration = 13E3;
 
 void setup() {
-  pinMode(BUZZER_PIN, OUTPUT);
-  tone(BUZZER_PIN, BUZZER_FREQUENCY, BUZZER_DURATION);
+  pinMode(buzzer_pin, OUTPUT);
+  tone(buzzer_pin, buzzer_frequency, buzzer_duration);
 }
 
 void loop() {
